86bcf6f3f56b8adc784f80f8df7355a3399266b0
seodh00 <seo.dh@deeplehr.com>
2024-10-23 21:37:46 +0900

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true,
-    },
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
