82644fb41e757524f2e908ed9c09fe1f72927669
seodh00 <seo.dh@deeplehr.com>
2024-10-28 17:33:58 +0900

    pwa dev disable

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
-  //disable: process.env.NODE_ENV === 'development'
-  disable: false
+  disable: process.env.NODE_ENV === 'development'
+  //disable: false
 })
 
 /** @type {import('next').NextConfig} */
